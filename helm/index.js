e612313ee47d3b5e5a51991f21cf71c6dbbd49e0
Casey Morford <casey@bigblueswimschool.com>
2020-05-20 16:05:36 -0500

    Test no inputs

--- helm/index.js
+++ helm/index.js
@@ -1,50 +1,22 @@
 const core = require("@actions/core");
 const github = require("@actions/github");
 const exec = require("@actions/exec");
-const required = { required: true };
-
-function getValues(values) {
-    if (!values) {
-        return "{}";
-    }
-    if (typeof values === "object") {
-        return JSON.stringify(values);
-    }
-    return values;
-}
-
-function getInput(name, options) {
-  const context = github.context;
-  const deployment = context.payload.deployment;
-  let val = core.getInput(name.replace("_", "-"), {
-    ...options,
-    required: false
-  });
-  // if (deployment) {
-  //   if (deployment[name]) val = deployment[name];
-  //   if (deployment.payload[name]) val = deployment.payload[name];
-  // }
-  if (options && options.required && !val) {
-    throw new Error(`Input required and not supplied: ${name}`);
-  }
-  return val;
-}
 
 /**
  * Run executes the helm deployment.
  */
 async function run() {
     try {
-      const context = github.context;  
-      const appName = getInput("release", required);
-      const namespace = getInput("namespace", required);
-      const chart = `/usr/app/charts/${getInput("chart", required)}`;
-      const values = getValues(getInput("values"));
+      // const context = github.context;  
+      // const appName = getInput("release", required);
+      // const namespace = getInput("namespace", required);
+      // const chart = `/usr/app/charts/${getInput("chart", required)}`;
+      // const values = getValues(getInput("values"));
       
-      core.debug(`param: release = "${appName}"`);
-      core.debug(`param: namespace = "${namespace}"`);
-      core.debug(`param: chart = "${chart}"`);
-      core.debug(`param: values = "${values}"`);
+      // core.debug(`param: release = "${appName}"`);
+      // core.debug(`param: namespace = "${namespace}"`);
+      // core.debug(`param: chart = "${chart}"`);
+      // core.debug(`param: values = "${values}"`);
   
       // Setup command options and arguments.
       const args = [
