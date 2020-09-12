33ca95cc75ec68e1bddd25e78d1ccc7bab712126
Casey Morford <casey@bigblueswimschool.com>
2020-09-12 09:22:08 -0500

    Update build

--- build-number/index.js
+++ build-number/index.js
@@ -1,31 +1,33 @@
+const axios = require('axios')
+
+const fail = (message, exitCode = 1) => {
+  console.log(`::error::${message}`);
+  process.exit(exitCode);
+}
+
 async function run() {
   try {
     const path = '.build_number';
-    const offset = process.env.INPUT_OFFSET ? parseInt(process.env.INPUT_OFFSET) : 0;
-
+    const prefix = process.env.INPUT_PREFIX ? `${process.env.INPUT_OFFSET}-` : '';
+    
+    if (process.env.INPUT_PREFIX) {
+      console.log(`Using Prefix ${prefix}...`)
+    }
+    
     //See if we've already generated the build number and are in later steps...
     if (fs.existsSync(path)) {
         let buildNumber = fs.readFileSync(path);
-        console.log(`Build number already generated in earlier jobs, using build number ${buildNumber}`);
+        console.log(`Build number already generated: ${buildNumber}`);
         console.log(`::set-env name=BUILD_NUMBER::${buildNumber}`);
         console.log(`::set-output name=build_number::${buildNumber}`);
         return;
     }
 
-    for (let varName of ['GITHUB_RUN_NUMBER']) {
+    for (let varName of ['INPUT_TOKEN', 'GITHUB_REPOSITORY', 'GITHUB_SHA']) {
       if (!process.env[varName]) {
           fail(`ERROR: Environment variable ${varName} is not defined.`);
       }
     }
-
-    const runNumber = parseInt(process.env.GITHUB_RUN_NUMBER)
-    const buildNumber = runNumber + offset
-
-    console.log(`Calculated Build number: ${buildNumber}`);
-    console.log(`::set-env name=BUILD_NUMBER::${buildNumber}`);
-    console.log(`::set-output name=build_number::${buildNumber}`);
-
-    fs.writeFileSync(path, nextBuildNumber.toString());
     
   } catch (error) {
     core.error(error);
