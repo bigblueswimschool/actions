d55803fb0df84fdc08273bfa709e98b9e74fbfff
Casey Morford <casey@bigblueswimschool.com>
2020-09-12 09:44:33 -0500

    Get last build number

--- build-number/index.js
+++ build-number/index.js
@@ -2,30 +2,50 @@ const fs = require('fs');
 const core = require("@actions/core");
 const axios = require('axios');
 
+const tagPrefix = 'build-'
+const GITHUB_REPOSITORY = process.env.GITHUB_REPOSITORY || null
+
+const github = axios.create({
+  baseURL: 'https://api.github.com',
+  headers: {
+    'Authorization' : `token ${process.env.INPUT_TOKEN}`
+  }
+});
+
+const getLastBuildNumber = async (prefix) => {
+  const response = await github.get(`/repos/${GITHUB_REPOSITORY}/git/refs/tags/${prefix}${tagPrefix}`);
+  console.log(response.data)
+  return 1
+}
+
 async function run() {
   try {
     const path = '.build_number';
     const prefix = process.env.INPUT_PREFIX ? `${process.env.INPUT_PREFIX}-` : '';
     
-    if (process.env.INPUT_PREFIX) {
+    if (prefix.length > 0) {
       console.log(`Using Prefix ${prefix}...`)
     }
 
-    //See if we've already generated the build number and are in later steps...
+    // See if we've already generated a build number
     if (fs.existsSync(path)) {
-        let buildNumber = fs.readFileSync(path);
+        const buildNumber = fs.readFileSync(path);
         console.log(`Build number already generated: ${buildNumber}`);
         console.log(`::set-env name=BUILD_NUMBER::${buildNumber}`);
         console.log(`::set-output name=build_number::${buildNumber}`);
         return;
     }
 
+    // Check environment vars
     for (let varName of ['INPUT_TOKEN', 'GITHUB_REPOSITORY', 'GITHUB_SHA']) {
       if (!process.env[varName]) {
           const error = new Error(`ERROR: Environment variable ${varName} is not defined.`)
           throw error
       }
     }
+
+    const lastBuildNumber = await getLastBuildNumber(prefix);
+    console.log(`Previous Build Number: ${lastBuildNumber}`);
     
   } catch (error) {
     core.error(error);
